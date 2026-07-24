b342d46d9402b8659b97c67224095337ce172a17
Hérick Bittencourt <professional.herickvvb@gmail.com>
2026-07-24 08:56:05 -0300

    feat: Adicionar base p/ M1 (Passeio do Cavalo)

---
+++ M1/alg-estrategia-cantos.cpp
@@ -0,0 +1,5 @@
+// Este código implementa um algoritmo estratégico para resolver
+// o passeio do cavalo no tabuleiro de xadrez. 
+// O objetivo é encontrar um caminho que permita ao cavalo visitar todas as casas do tabuleiro exatamente uma vez.
+// O algoritmo utiliza uma abordagem heurística, priorizando movimentos que levam a casas com menos opções de saída, 
+// aumentando assim as chances de completar o passeio com sucesso.
---
+++ M1/alg-forca-bruta.cpp
@@ -0,0 +1,3 @@
+// Este código implementa um algoritmo de força bruta (aleatório) para resolver
+// o passeio do cavalo no tabuleiro de xadrez. 
+// O objetivo é encontrar um caminho que permita ao cavalo visitar todas as casas do tabuleiro exatamente uma vez.
